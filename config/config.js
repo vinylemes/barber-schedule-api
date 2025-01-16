caaf34ee6855c3aa9b95799a83be022d2a634e3b
Vinicius Lemes Alves <vlemesalves@gmail.com>
2025-01-15 21:59:15 -0300

    Add staging on config.js

--- config/config.js
+++ config/config.js
@@ -9,6 +9,14 @@ module.exports = {
     port: process.env.DATABASE_PORT || 5432,
     dialect: 'postgres',
   },
+  staging: {
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    dialect: 'postgres',
+  },
   production: {
     username: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
